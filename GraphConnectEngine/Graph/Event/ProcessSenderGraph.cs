787b911abd603437a833847e2467e4d1361a5cf4
nananapo <kanapodream@gmail.com>
2021-09-28 21:56:35 +0900

    ProcessSender restriction

--- GraphConnectEngine/Graph/Event/ProcessSenderGraph.cs
+++ GraphConnectEngine/Graph/Event/ProcessSenderGraph.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using GraphConnectEngine.Core;
 using GraphConnectEngine.Node;
 
@@ -6,14 +7,24 @@ namespace GraphConnectEngine.Graph.Event
     public abstract class ProcessSenderGraph : GraphBase
     {
         
+        //TODO 実行制限 : キャッシュの処理を加えるまで
+        //TODO 1秒ごと,1秒後に
+        private bool _isProcessing = false;
+        
         public ProcessSenderGraph(NodeConnector connector) : base(connector)
         {
         }
 
-        protected virtual void Fire()
+        protected virtual async Task Fire()
         {
+            if (_isProcessing)
+                return;
+
             Logger.Debug("---------------------------------------------------------------------");
-            OutProcessNode.CallProcess(ProcessCallArgs.Fire(GetHashCode()));
+            
+            _isProcessing = true;
+            await OutProcessNode.CallProcess(ProcessCallArgs.Fire(GetHashCode()));
+            _isProcessing = false;
         }
 
         public override bool IsConnectedInProcessNode()
