c5de44af5db3b86a6dde53ced296cf9b93e65f22
nananapo <kanapodream@gmail.com>
2021-10-17 00:50:52 +0900

    INodeConnectorAsyncをINodeに対応

--- GraphConnectEngine/Node/INodeConnectorAsync.cs
+++ GraphConnectEngine/Node/INodeConnectorAsync.cs
@@ -14,33 +14,31 @@ namespace GraphConnectEngine.Node
         /// <param name="key"></param>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
-        Task<T[]> GetOtherNodesAsync<T>(Node key);
+        Task<T[]> GetOtherNodesAsync<T>(INode key);
 
         /// <summary>
         /// 繋がれているノードを取得する
         /// </summary>
         /// <param name="key"></param>
         /// <returns></returns>
-        Task<Node[]> GetOtherNodesAsync(Node key);
+        Task<INode[]> GetOtherNodesAsync(INode key);
 
         /// <summary>
         /// 繋がれているノードをキャストして取得する
         /// 繋がれていない場合はfalseを返す
         /// </summary>
         /// <param name="key"></param>
-        /// <param name="result"></param>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
-        Task<bool> TryGetOtherNodesAsync<T>(Node key, out T[] result);
+        Task<ValueResult<T[]>> TryGetOtherNodesAsync<T>(INode key);
 
         /// <summary>
         /// 繋がれているノードを取得する
         /// 繋がれていない場合はfalseを返す
         /// </summary>
         /// <param name="key"></param>
-        /// <param name="result"></param>
         /// <returns></returns>
-        Task<bool> TryGetOtherNodesAsync(Node key, out Node[] result);
+        Task<ValueResult<INode[]>> TryGetOtherNodesAsync(INode key);
         
         /// <summary>
         /// 繋がれているノードをキャストして取得する
@@ -50,15 +48,14 @@ namespace GraphConnectEngine.Node
         /// <param name="result"></param>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
-        Task<bool> TryGetAnotherNodeAsync<T>(Node key, out T result) where T : Node;
+        Task<ValueResult<T>> TryGetAnotherNodeAsync<T>(INode key) where T : class;
         /// <summary>
         /// 繋がれているノードを取得する
         /// 繋がれていない場合はfalseを返す
         /// </summary>
         /// <param name="key"></param>
-        /// <param name="result"></param>
         /// <returns></returns>
-        Task<bool> TryGetAnotherNodeAsync(Node key, out Node result);
+        Task<ValueResult<INode>> TryGetAnotherNodeAsync(INode key);
 
         /// <summary>
         /// node1がnode2と繋がっているかチェック(node1のみ)
@@ -66,7 +63,7 @@ namespace GraphConnectEngine.Node
         /// <param name="node1"></param>
         /// <param name="node2"></param>
         /// <returns></returns>
-        Task<bool> IsConnectedAsync(Node node1, Node node2);
+        Task<bool> IsConnectedAsync(INode node1, INode node2);
 
         /// <summary>
         /// ノードとノードを繋ぐ
@@ -74,7 +71,7 @@ namespace GraphConnectEngine.Node
         /// <param name="node1"></param>
         /// <param name="node2"></param>
         /// <returns></returns>
-        Task<bool> ConnectNodeAsync(Node node1, Node node2);
+        Task<bool> ConnectNodeAsync(INode node1, INode node2);
 
         /// <summary>
         /// ノードを切断する
@@ -82,13 +79,13 @@ namespace GraphConnectEngine.Node
         /// <param name="node1"></param>
         /// <param name="node2"></param>
         /// <returns></returns>
-        Task<bool> DisconnectNodeAsync(Node node1, Node node2);
+        Task<bool> DisconnectNodeAsync(INode node1, INode node2);
 
         /// <summary>
         /// 指定されたノードの接続情報を消す
         /// </summary>
         /// <param name="node"></param>
         /// <returns></returns>
-        Task<bool> DisconnectAllNodeAsync(Node node);
+        Task<bool> DisconnectAllNodeAsync(INode node);
     }
 }
