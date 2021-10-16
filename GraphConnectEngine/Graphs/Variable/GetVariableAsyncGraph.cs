780765324bc6d29a587e1b302c736e100c8ed3cd
nananapo <kanapodream@gmail.com>
2021-10-16 10:06:26 +0900

    Created Get/SetVariableAsyncGraph

---
+++ GraphConnectEngine/Graphs/Variable/GetVariableAsyncGraph.cs
@@ -0,0 +1,62 @@
+using System.Threading.Tasks;
+using GraphConnectEngine.Node;
+using GraphConnectEngine.Variable;
+
+namespace GraphConnectEngine.Graphs.Variable
+{
+    /// <summary>
+    /// IVariableHolderAsyncの変数を取得するグラフ
+    ///
+    /// OutItemNode[0]に変数が渡される
+    /// </summary>
+    public class GetVariableAsyncGraph : VariableAsyncGraph
+    {
+
+        public GetVariableAsyncGraph(NodeConnector connector,IVariableHolderAsync holder) : base(connector,holder)
+        {
+            AddNode(new OutItemNode(this, typeof(void), 0,"Value"));
+        }
+
+
+        public override async Task<ProcessCallResult> OnProcessCall(ProcessCallArgs args, object[] parameters)
+        {
+            //Holderがない
+            if (Holder == null)
+                return ProcessCallResult.Fail();
+
+            //取得
+            var result = await Holder.TryGetAsync(VariableName);
+            
+            if(!result.IsSucceeded)
+                return ProcessCallResult.Fail();
+
+            return ProcessCallResult.Success(new [] {result.Value}, OutProcessNode);
+        }
+
+        protected override async void OnVariableChanged()
+        {
+            if (Holder == null)
+                return;
+            
+            if (await Holder.ContainsKeyAsync(VariableName))
+            {
+                var rs = await Holder.TryGetVariableTypeAsync(VariableName);
+                OutItemNodes[0].SetItemType(rs.Value);
+            }
+        }
+
+        protected override async void OnHolderChanged()
+        {
+            if (Holder == null)
+                return;
+            
+            if (await Holder.ContainsKeyAsync(VariableName))
+            {
+                var rs = await Holder.TryGetVariableTypeAsync(VariableName);
+                OutItemNodes[0].SetItemType(rs.Value);
+            }
+        }
+
+        public override string GetGraphName() => "Get Variable Graph";
+    }
+}
---
+++ GraphConnectEngine/Graphs/Variable/SetVariableAsyncGraph.cs
@@ -0,0 +1,62 @@
+using System.Threading.Tasks;
+using GraphConnectEngine.Node;
+using GraphConnectEngine.Variable;
+
+namespace GraphConnectEngine.Graphs.Variable
+{
+    /// <summary>
+    /// IVariableHolderAsyncの変数の値を設定するグラフ
+    /// </summary>
+    public class SetVariableAsyncGraph : VariableAsyncGraph
+    {
+
+        public SetVariableAsyncGraph(NodeConnector connector, IVariableHolderAsync holder) : base(connector,holder)
+        {
+            AddNode(new InItemNode(this, typeof(void),"Value"));
+            AddNode(new OutItemNode(this,typeof(void),0,"Value"));
+        }
+
+        public override async Task<ProcessCallResult> OnProcessCall(ProcessCallArgs args, object[] parameters)
+        {
+            if (Holder == null)
+                return ProcessCallResult.Fail();
+
+            var obj = parameters[0];
+            
+            //更新
+            if (!await Holder.UpdateAsync(VariableName, obj))
+                return ProcessCallResult.Fail();
+
+            return ProcessCallResult.Success(new[] {obj}, OutProcessNode);
+        }
+
+        protected override async void OnVariableChanged()
+        {
+            if (Holder == null)
+                return;
+
+            if (await Holder.ContainsKeyAsync(VariableName))
+            {
+                var rs = await Holder.TryGetVariableTypeAsync(VariableName);
+                InItemNodes[0].SetItemType(rs.Value);
+                OutItemNodes[0].SetItemType(rs.Value);
+            }
+        }
+
+        protected override async void OnHolderChanged()
+        {
+            if (Holder == null)
+                return;
+            
+            if (await Holder.ContainsKeyAsync(VariableName))
+            {
+                var rs = await Holder.TryGetVariableTypeAsync(VariableName);
+                InItemNodes[0].SetItemType(rs.Value);
+                OutItemNodes[0].SetItemType(rs.Value);
+            }
+        }
+        
+        public override string GetGraphName() => "Set Variable Graph";
+        
+    }
+}
