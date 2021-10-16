fdd28e61ba7540d94e0750567c71aabd36b7d8f3
nananapo <kanapodream@gmail.com>
2021-10-16 10:05:58 +0900

    Created VariableAsyncGraph

---
+++ GraphConnectEngine/Graphs/Variable/VariableAsyncGraph.cs
@@ -0,0 +1,64 @@
+using GraphConnectEngine.Node;
+using GraphConnectEngine.Variable;
+
+namespace GraphConnectEngine.Graphs.Variable
+{
+    /// <summary>
+    /// IVariableHolderAsyncを使うグラフ
+    /// 抽象クラス
+    /// </summary>
+    public abstract class VariableAsyncGraph : Graph
+    {
+
+        private IVariableHolderAsync _holder;
+
+        /// <summary>
+        /// IVariableHolderのプロパティ
+        /// </summary>
+        public IVariableHolderAsync Holder
+        {
+            get => _holder;
+            private set
+            {
+                _holder = value;
+                OnHolderChanged();
+            }
+        }
+
+        private string _variableName = "";
+
+        /// <summary>
+        /// 変数名のプロパティ
+        /// </summary>
+        public string VariableName
+        {
+            get => _variableName;
+            set
+            {
+                _variableName = value;
+                OnVariableChanged();
+            }
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="connector"></param>
+        /// <param name="holder">IVariableHolder</param>
+        protected VariableAsyncGraph(NodeConnector connector, IVariableHolderAsync holder) : base(connector)
+        {
+            _holder = holder;
+        }
+
+        /// <summary>
+        /// 変数名が変わった時に呼ばれる
+        /// </summary>
+        protected abstract void OnVariableChanged();
+
+        /// <summary>
+        /// IVariableHolderが変わったら呼ばれる
+        /// </summary>
+        protected abstract void OnHolderChanged();
+        
+    }
+}
