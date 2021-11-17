6360c7454b2d71059b4c4c259a084cff023a59cc
nananapo <nananapo@users.noreply.github.com>
2021-11-17 05:54:41 +0000

    deploy: 0900900d9b44cfeae0fbca3106a0851799d5e80d

--- search/all_2.js
+++ search/all_2.js
@@ -9,5 +9,5 @@ var searchData=
   ['disconnectnode_17',['DisconnectNode',['../interface_graph_connect_engine_1_1_nodes_1_1_i_node_connector.html#a6da6e2c2b2e67aa03a54f5aac5baec7f',1,'GraphConnectEngine.Nodes.INodeConnector.DisconnectNode()'],['../class_graph_connect_engine_1_1_nodes_1_1_node_connector.html#ab0cebab540ef87c80ea852fe6ffafada',1,'GraphConnectEngine.Nodes.NodeConnector.DisconnectNode()']]],
   ['disconnectnodeasync_18',['DisconnectNodeAsync',['../interface_graph_connect_engine_1_1_nodes_1_1_i_node_connector_async.html#aac65bfe621b694cd9debd473607731a5',1,'GraphConnectEngine::Nodes::INodeConnectorAsync']]],
   ['dispose_19',['Dispose',['../class_graph_connect_engine_1_1_graph.html#ac6c4e822b50711dc1eb7ceb29641b851',1,'GraphConnectEngine.Graph.Dispose()'],['../class_graph_connect_engine_1_1_nodes_1_1_node.html#a160093d9db1fc15be7fc7ccd522be1eb',1,'GraphConnectEngine.Nodes.Node.Dispose()']]],
-  ['dumpnode_20',['DumpNode',['../class_graph_connect_engine_1_1_nodes_1_1_node_connector.html#a81ecc346dc0083488aa76a48491f9c2d',1,'GraphConnectEngine::Nodes::NodeConnector']]]
+  ['dumpnode_20',['DumpNode',['../class_graph_connect_engine_1_1_logger.html#a17443cce42a77fdd85b40446424f71f9',1,'GraphConnectEngine::Logger']]]
 ];
--- search/functions_2.js
+++ search/functions_2.js
@@ -8,5 +8,5 @@ var searchData=
   ['disconnectnode_229',['DisconnectNode',['../interface_graph_connect_engine_1_1_nodes_1_1_i_node_connector.html#a6da6e2c2b2e67aa03a54f5aac5baec7f',1,'GraphConnectEngine.Nodes.INodeConnector.DisconnectNode()'],['../class_graph_connect_engine_1_1_nodes_1_1_node_connector.html#ab0cebab540ef87c80ea852fe6ffafada',1,'GraphConnectEngine.Nodes.NodeConnector.DisconnectNode()']]],
   ['disconnectnodeasync_230',['DisconnectNodeAsync',['../interface_graph_connect_engine_1_1_nodes_1_1_i_node_connector_async.html#aac65bfe621b694cd9debd473607731a5',1,'GraphConnectEngine::Nodes::INodeConnectorAsync']]],
   ['dispose_231',['Dispose',['../class_graph_connect_engine_1_1_graph.html#ac6c4e822b50711dc1eb7ceb29641b851',1,'GraphConnectEngine.Graph.Dispose()'],['../class_graph_connect_engine_1_1_nodes_1_1_node.html#a160093d9db1fc15be7fc7ccd522be1eb',1,'GraphConnectEngine.Nodes.Node.Dispose()']]],
-  ['dumpnode_232',['DumpNode',['../class_graph_connect_engine_1_1_nodes_1_1_node_connector.html#a81ecc346dc0083488aa76a48491f9c2d',1,'GraphConnectEngine::Nodes::NodeConnector']]]
+  ['dumpnode_232',['DumpNode',['../class_graph_connect_engine_1_1_logger.html#a17443cce42a77fdd85b40446424f71f9',1,'GraphConnectEngine::Logger']]]
 ];
